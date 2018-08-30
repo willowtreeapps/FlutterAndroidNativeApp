919a97150040cdf12155b7310a144d5caed65258
Kurian Vithayathil <k.vithayathil@gmail.com>
2018-08-30 11:38:00 -0400

    - Add button to activate flutter view (WIP)

--- app/src/main/java/me/osama/flutternativeapp/MainActivity.kt
+++ app/src/main/java/me/osama/flutternativeapp/MainActivity.kt
@@ -2,11 +2,24 @@ package me.osama.flutternativeapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.FrameLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import io.flutter.facade.Flutter
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fab.setOnClickListener { navigateFlutter() }
+    }
+
+
+    private fun navigateFlutter() {
+        val tx = supportFragmentManager.beginTransaction()
+        tx.replace(R.id.container, Flutter.createFragment("route1"))
+        tx.commit()
     }
 }
